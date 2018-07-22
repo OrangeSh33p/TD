97021353321e97170504c00f5deb14abbcea44f3
LAPTOP-MS4J8971\couvr <couvreurhenri@gmail.com>
2018-07-22 19:43:10 +0200

    push project

---
+++ Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+	[HideInInspector] public GameObject target;
+	[HideInInspector] public float speed;
+	[HideInInspector] public float damage;
+
+	void Update () 
+	{
+		if (target == null)
+		{
+			Destroy (gameObject);
+		}
+
+		transform.LookAt (target.transform);
+		transform.position += transform.forward*speed*Time.deltaTime;
+
+		if (Vector3.Distance(transform.position, target.transform.position) < 1)
+		{
+			target.GetComponent<Monstre> ().Damaged (damage);
+			Destroy (gameObject);		
+		}
+	}
+}
---
+++ Project/Assets/Scripts/Monstre.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monstre : MonoBehaviour 
+{
+	public float speed;
+	public float maxHp;
+	public float hp;
+
+	public Slider hpBar;
+
+	void Start ()
+	{
+		hp = maxHp;
+	}
+
+	void Update ()
+	{
+		transform.position += transform.forward*speed*Time.deltaTime;
+	}
+
+	public void Damaged (float damage)
+	{
+		hp -= damage;
+		hpBar.value = hp / maxHp;
+		if (hp == 0) Destroy (transform.parent.gameObject);
+	}
+}
